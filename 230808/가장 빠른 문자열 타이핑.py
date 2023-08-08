47670240760ab6c8b8ddbaeaa068b4fce19d8776
suminjeff <suminjeff@gmail.com>
2023-08-09 00:58:02 +0900

    commit

--- 230808/가장 빠른 문자열 타이핑.py
+++ 230808/가장 빠른 문자열 타이핑.py
@@ -1,6 +1,7 @@
 import sys
 
-sys.stdin = open("회문1.txt", "r", encoding="utf-8")
+sys.stdin = open("가장 빠른 문자열 타이핑.txt", "r", encoding="utf-8")
+
 
 def my_len(arr):
     count = 0
@@ -9,14 +10,16 @@ def my_len(arr):
     return count
 
 
-def brute_force(target, pattern):
+def brute_force(t, p):
     t_idx = 0
     p_idx = 0
 
-    t_len = my_len(target)
-    p_len = my_len(pattern)
-    while (t_idx < t_len) and (p_idx < p_len):
-        if target[t_idx] == pattern[p_idx]:
+    t_len = my_len(t)
+    p_len = my_len(p)
+
+    checklist = []
+    while t_idx < t_len and p_idx < p_len:
+        if t[t_idx] == p[p_idx]:
             t_idx += 1
             p_idx += 1
         else:
@@ -24,13 +27,15 @@ def brute_force(target, pattern):
             p_idx = 0
 
     if p_idx == p_len:
-        return t_idx - p_idx
+        checklist.append(t_idx - p_idx)
     else:
-        return -1
+        return False
+    return checklist
 
 
 T = int(input())
 for tc in range(1, T+1):
-    t = input()
-    p = input()
-    print(brute_force(t, p))
+    t, p = input().split()
+    start = 0
+    check_list = brute_force(t, p)
+    while start < my_len(t):
