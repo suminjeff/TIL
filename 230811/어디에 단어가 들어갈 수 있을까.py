6cf024fa0bebce9bfced2c031510700344297714
suminjeff <suminjeff@gmail.com>
2023-08-12 23:37:58 +0900

    230811

--- 230811/어디에 단어가 들어갈 수 있을까.py
+++ 230811/어디에 단어가 들어갈 수 있을까.py
@@ -8,25 +8,28 @@ for tc in range(1, T+1):
     N, K = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
 
-    cnt = 1
+    cnt = 0
     for row in range(N):
-        temp = 1
-        for col in range(N - 1):
-            if arr[row][col] == arr[row][col + 1] == 1:
-                temp += 1
-            else:
-                temp = 1
-        if temp == K:
-            cnt += 1
+        space = 0
+        for col in range(N):
+            if arr[row][col] == 1:
+                space += 1
+            if arr[row][col] == 0 or col == N-1:
+                if space == K:
+                    cnt += 1
+                if arr[row][col] == 0:
+                    space = 0
+        # if temp == K:
+        #     cnt += 1
 
     for col in range(N):
-        temp = 1
-        for row in range(N - 1):
-            if arr[row][col] == arr[row+1][col] == 1:
-                temp += 1
-            else:
-                temp = 1
-        if temp == K:
-            cnt += 1
-
+        space = 0
+        for row in range(N):
+            if arr[row][col] == 1:
+                space += 1
+            if arr[row][col] == 0 or row == N-1:
+                if space == K:
+                    cnt += 1
+                if arr[row][col] == 0:
+                    space = 0
     print(f"#{tc} {cnt}")
