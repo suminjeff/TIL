0ef76b94267a86ff431e4f335206a9ecc53fd567
suminjeff <suminjeff@gmail.com>
2023-08-16 23:30:11 +0900

    IM

--- 00_algorithm_study/8월 3주/목차 세기.py
+++ 00_algorithm_study/8월 3주/목차 세기.py
@@ -2,22 +2,41 @@ import sys
 
 sys.stdin = open("목차 세기.txt", "r")
 
-def check(arr):
-    N = len(arr)
-    vertex = arr.count(1)
-    idx = 0
-    for v in range(vertex):
-        tree = []
-        while idx < N-1:
+
+def push(item):
+    global top
+    global N
+
+    top += 1
+    if top == N:
+        print('overflow')
+    else:
+        stack[top] = item
 
 
+def pop():
+    global top
+    if top == -1:
+        print('underflow')
+        return 0
+    else:
+        top -= 1
+        return stack[top+1]
 
 
+N = int(input())
+arr = [int(input()) for _ in range(N)]
 
+stack = [0] * N
+top = -1
 
-levels = []
-for _ in range(int(input())):
-    levels.append(int(input()))
-vertex = levels.count(1)
-print(levels)
-print(vertex)
+# for i in arr:
+#     if not stack:
+#         stack.append(i)
+#     else:
+#         if i == 1:
+#             stack.pop()
+#     elif
+push(arr[0])
+
+print(stack)
