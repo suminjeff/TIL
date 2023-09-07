0d24999687c31286db92a5d92d591b6b8e9337ca
suminjeff <suminjeff@gmail.com>
2023-09-08 00:12:38 +0900

    230908

---
+++ 00_Ad/practice/단어 공부.py
@@ -0,0 +1,16 @@
+import sys
+
+sys.stdin = open("단어 공부.txt", "r")
+
+word = input().upper()
+wordset = set(word)
+max_letter = ""
+max_cnt = 0
+for letter in wordset:
+    cnt = word.count(letter)
+    if max_cnt == cnt:
+        max_letter = "?"
+    elif max_cnt < cnt:
+        max_cnt = cnt
+        max_letter = letter
+print(max_letter)
---
+++ 00_Ad/practice/크로아티아 알파벳.py
@@ -0,0 +1,17 @@
+import sys
+
+sys.stdin = open("크로아티아 알파벳.txt", "r")
+
+alphabet = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+checker = ["c", "d", "l", "n", "s", "z"]
+input_word = input()
+N = len(input_word)
+
+cro1 = []
+cro2 = ""
+i = 0
+l
+
+
+
+print(cnt)
