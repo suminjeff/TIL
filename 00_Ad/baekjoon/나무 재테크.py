35c61c2ba632730f95aa20986e79b67bfe51bef1
suminjeff <suminjeff@gmail.com>
2023-09-08 17:57:40 +0900

    230908

--- 00_Ad/baekjoon/나무 재테크.py
+++ 00_Ad/baekjoon/나무 재테크.py
@@ -5,57 +5,82 @@ sys.stdin = open("나무 재테크.txt", "r")
 start_time = time.time()
 
 
+def spring():
+    for r in range(N):
+        for c in range(N):
+            if age_list[r][c]:
+                length = len(age_list[r][c])
+                for i in range(length):
+                    if age_list[r][c][i]:
+                        if soil[r][c] - age_list[r][c][i] >= 0:
+                            soil[r][c] -= age_list[r][c][i]
+                            age_list[r][c][i] += 1
+                        else:
+                            age_list[r][c][i] = str(age_list[r][c][i])
+
+
+def summer():
+    for r in range(N):
+        for c in range(N):
+            if age_list[r][c]:
+                length = len(age_list[r][c])
+                for i in range(length):
+                    if type(age_list[r][c][i]) == str:
+                        soil[r][c] += int(age_list[r][c][i])//2
+                        age_list[r][c][i] = 0
+
+
+def fall():
+    for r in range(N):
+        for c in range(N):
+            if age_list[r][c]:
+                length = len(age_list[r][c])
+                for i in range(length):
+                    if age_list[r][c][i] > 0 and age_list[r][c][i] % 5 == 0:
+                        for k in range(8):
+                            nr = r + dr[k]
+                            nc = c + dc[k]
+                            if 0 <= nr < N and 0 <= nc < N:
+                                age_list[nr][nc].append(1)
+                                age_list[nr][nc].sort()
+
+
+def winter():
+    for r in range(N):
+        for c in range(N):
+            soil[r][c] += input_arr[r][c]
+
+
 N, M, K = map(int, input().split())
 input_arr = [list(map(int, input().split())) for _ in range(N)]
 
 soil = [[5]*N for _ in range(N)]
-trees = []
-front = rear = -1
-year = 0
+age_list = [[[] for _ in range(N)] for _ in range(N)]
 
 for _ in range(M):
     x, y, z = map(int, input().split())
-    trees.append((x-1, y-1, [z], []))
-    rear += 1
+    age_list[x-1][y-1].append(z)
 
 dr = [-1, -1, -1, 0, 0, 1, 1, 1]
 dc = [-1, 0, 1, -1, 1, -1, 0, 1]
 
 for year in range(K):
-    # spring
-    for tree in trees:
-        r, c, age_list, dead = tree
-        front += 1
-        age_list.sort()
-        for i in range(len(age_list)):
-            if age_list[i]:
-                if soil[r][c] - age_list[i] >= 0:
-                    soil[r][c] -= age_list[i]
-                    age_list[i] += 1
-                else:
-                    dead.append(str(age_list[i]))
-                    age_list[i] = 0
-        trees.append((r, c, age_list, dead))
-        rear += 1
+    spring()
+    summer()
+    fall()
+    winter()
 
-    # summer
-    for tree in trees:
-        r, c, age_list, dead = tree
-        for dead_tree in dead:
-            for k in range(8):
-                nr = r + dr[k]
-                nc = c + dc[k]
-                if 0 <= nr < N and 0 <= nc < N:
+ans = 0
+for r in range(N):
+    for c in range(N):
+        if age_list[r][c]:
+            length = len(age_list[r][c])
+            for i in range(length):
+                if age_list[r][c][i] != 0:
+                    ans += 1
 
 
-
-
-
-
-print(input_arr)
-print(soil)
-print(trees)
-print(age_list)
+print(ans)
 
 end_time = time.time()
 print("time :", end_time - start_time)
