b9cff7990f89d95ceb7873d992982893f7e5081d
suminjeff <suminjeff@gmail.com>
2023-10-06 00:18:16 +0900

    231006

--- 00_Ad/baekjoon/괄호 추가하기.py
+++ 00_Ad/baekjoon/괄호 추가하기.py
@@ -1,9 +1,77 @@
 import sys
-
 sys.stdin = open("괄호 추가하기.txt", "r")
+input = sys.stdin.readline
+import time
+
+st = time.time()
+
+def calc(arr):
+    stack = []
+    i = 0
+    while i < len(arr):
+        if stack:
+            op = arr[i]
+            i += 1
+            if op == "+":
+                stack[-1] += arr[i]
+            elif op == "-":
+                stack[-1] -= arr[i]
+            elif op == "*":
+                stack[-1] *= arr[i]
+            i += 1
+        else:
+            stack.append(arr[i])
+            i += 1
+    return stack[-1]
+
+
+def prioritize(ops_idx):
+    res = []
+    i = 0
+    while i < N:
+        if type(arr[i]) == int:
+            res.append(arr[i])
+            i += 1
+        else:
+            op = input_arr[i]
+            if i in ops_idx:
+                if op == "+":
+                    res.append(res.pop()+arr[i+1])
+                elif op == "-":
+                    res.append(res.pop()-arr[i+1])
+                elif op == "*":
+                    res.append(res.pop()*arr[i+1])
+                i += 2
+            else:
+                res.append(arr[i])
+                i += 1
+    return calc(res)
+
 
 N = int(input())
-phrase = input()
-max_brackets = (N//2+1)//2
+input_arr = list(input())
+arr = []
+for tkn in input_arr:
+    if tkn.isnumeric():
+        arr.append(int(tkn))
+    else:
+        arr.append(tkn)
+max_v = calc(arr)
 
-print(phrase)
+ops = [2*i+1 for i in range(N//2)]
+for i in range(1, 1 << N//2):
+    subset = []
+    for j in range(N//2):
+        if i & (1 << j):
+            if subset:
+                if abs(ops[j]-subset[-1]) != 2:
+                    subset.append(ops[j])
+            else:
+                subset.append(ops[j])
+    print(subset)
+    max_v = max(max_v, prioritize(subset))
+
+print(max_v)
+
+et = time.time()
+print("time :", et-st)
--- 00_Ad/baekjoon/파이프 옮기기 1.py
+++ 00_Ad/baekjoon/파이프 옮기기 1.py
@@ -3,50 +3,68 @@ sys.stdin = open('파이프 옮기기 1.txt', 'r')
 input = sys.stdin.readline
 
 
+# def dfs(r, c, p):
+#     global cnt
+#     if r == c == N-1:
+#         cnt += 1
+#         return
+#
+#     for dr, dc, np in pipe[p]:
+#         nr, nc = r + dr, c + dc
+#         if np == 'd':
+#             if nr < N and nc < N and arr[nr][nc] == arr[nr-1][nc] == arr[nr][nc-1] == 0:
+#                 dfs(nr, nc, np)
+#         else:
+#             if nr < N and nc < N and arr[nr][nc] == 0:
+#                 dfs(nr, nc, np)
+#
+# pipe = {
+#     'h': [[0, 1, 'h'], [1, 1, 'd']],
+#     'v': [[1, 0, 'v'], [1, 1, 'd']],
+#     'd': [[0, 1, 'h'], [1, 0, 'v'], [1, 1, 'd']],
+# }
+#
+#
+# N = int(input())
+# arr = [list(map(int, input().rstrip().split())) for _ in range(N)]
+# cnt = 0
+#
+# if arr[N-1][N-1] == 1:
+#     print(cnt)
+# else:
+#     dfs(0, 1, 'h')
+#     print(cnt)
+#
+
+
 def dfs(r, c, p):
-    global cnt
-
+    print(r, c, memo[r][c])
     if r == c == N-1:
-        cnt += 1
-        return
-
+        return 1
+    if memo[r][c][p] != -1:
+        return memo[r][c][p]
+    total_cnt = 0
     for dr, dc, np in pipe[p]:
-        flag = True
-        for xr, xc in spaces[np]:
-            nxr, nxc = r + xr, c + xc
-            if 0 <= nxr < N and 0 <= nxc < N:
-                if arr[nxr][nxc] == 1:
-                    flag = False
-                    break
-        if flag:
-            nr, nc = r + dr, c + dc
-            if 0 <= nr < N and 0 <= nc < N and arr[nr][nc] == 0:
-                # print(nr, nc)
-                arr[nr][nc] = np
-                dfs(nr, nc, np)
-                arr[nr][nc] = 0
-
-
-spaces = {
-    'h': [[0, 1]],
-    'v': [[1, 0]],
-    'd': [[0, 1], [1, 0], [1, 1]],
-}
+        nr, nc = r + dr, c + dc
+        if np == 2:
+            if nr < N and nc < N and arr[nr][nc] == arr[nr-1][nc] == arr[nr][nc-1] == 0:
+                total_cnt += dfs(nr, nc, np)
+        else:
+            if nr < N and nc < N and arr[nr][nc] == 0:
+                total_cnt += dfs(nr, nc, np)
+    memo[r][c][p] = total_cnt
+    return total_cnt
 
 
 pipe = {
-    'h': [[0, 1, 'h'], [1, 1, 'd']],
-    'v': [[1, 0, 'v'], [1, 1, 'd']],
-    'd': [[0, 1, 'h'], [1, 0, 'v'], [1, 1, 'd']],
+    0: [[0, 1, 0], [1, 1, 2]],
+    1: [[1, 0, 1], [1, 1, 2]],
+    2: [[0, 1, 0], [1, 0, 1], [1, 1, 2]],
 }
-
 
 N = int(input())
 arr = [list(map(int, input().rstrip().split())) for _ in range(N)]
-arr[0][0] = arr[0][1] = 'h'
-cnt = 0
-if arr[N-1][N-1] == 1:
-    print(cnt)
-else:
-    dfs(0, 1, 'h')
-    print(cnt)
+memo = [[[-1 for _ in range(3)] for _ in range(N)] for _ in range(N)]
+result = dfs(0, 1, 0)
+
+print(result)
