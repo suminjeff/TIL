f16f637105966c76417b71cfa29f83e7d97a2e98
suminjeff <suminjeff@gmail.com>
2023-10-12 02:29:51 +0900

    231012

--- 00_Ad/practice/연구소.py
+++ 00_Ad/practice/연구소.py
@@ -2,61 +2,74 @@ import sys
 sys.stdin = open("연구소.txt", "r")
 input = sys.stdin.readline
 from collections import deque
-from copy import deepcopy
 
 
-def comb(s, k, e):
-    if s == e:
-        possible_arrangement.append(pick[:])
+def wall(depth):
+    if depth == 3:
+        virus()
         return
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 0:
+                arr[i][j] = 1
+                wall(depth+1)
+                arr[i][j] = 0
 
-    for i in range(k, len(sample)):
-        pick[s] = sample[i]
-        comb(s+1, i+1, e)
 
-
-def virus(picked):
-    global max_area
-    visited = [[0]*M for _ in range(N)]
-
-    for i in range(3):
-        r, c = picked[i]
-        arr[r][c] = 1
+def virus():
+    global max_cnt
+    z = zero_cnt
+    visited = [[False]*M for _ in range(N)]
+    que = deque()
+    for pr, pc in people:
+        que.append([pr, pc])
+        visited[pr][pc] = True
 
     while que:
         r, c = que.popleft()
-        visited[r][c] = 1
-        for dr, dc in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+        for dr, dc in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
             nr, nc = r + dr, c + dc
-            if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] == 0 and visited[nr][nc] == 0:
-                visited[nr][nc] = 1
+            if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] == 0 and visited[nr][nc] is False:
+                visited[nr][nc] = True
+                z -= 1
+                if z <= max_cnt:
+                    return
                 que.append([nr, nc])
-
-    area = 0
-    for i in range(N):
-        for j in range(M):
-            if arr[i][j] == 0 and visited[i][j] == 0:
-                area += 1
-    max_area = max(max_area, area)
-
+    max_cnt = max(max_cnt, z)
 
 
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-initial_area = 0
-max_area = 0
-que = deque()
-sample = []
+used = [[False]*M for _ in range(N)]
+people = []
+dq = deque()
+zero_cnt = -3
 for i in range(N):
     for j in range(M):
-        if arr[i][j] == 0:
-            sample.append([i, j])
-            initial_area += 1
-        elif arr[i][j] == 2:
-            que.append([i, j])
-pick = [[] for _ in range(3)]
-possible_arrangement = []
-comb(0, 0, 3)
-for pa in possible_arrangement:
-    virus(pa)
-print(max_area)
+        if arr[i][j] == 2:
+            people.append([i, j])
+            dq.append([i, j])
+            used[i][j] = True
+        elif arr[i][j] == 0:
+            zero_cnt += 1
+
+
+rows = []
+cols = []
+
+while dq:
+    r, c = dq.popleft()
+    for dr, dc in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
+        nr, nc = r + dr, c + dc
+        if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] == 0 and used[nr][nc] is False:
+            used[nr][nc] = True
+            rows.append(nr)
+            cols.append(nc)
+            dq.append([nr, nc])
+print(rows)
+print(cols)
+
+
+max_cnt = 0
+wall(0)
+print(max_cnt)
