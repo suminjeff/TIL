5d59f86da7b4cbaf3da237da0a1f363398d9eb2e
suminjeff <suminjeff@gmail.com>
2023-10-24 00:12:07 +0900

    231024

--- 00_Ad/practice/하노이 탑 이동 순서.py
+++ 00_Ad/practice/하노이 탑 이동 순서.py
@@ -5,19 +5,14 @@ input = sys.stdin.readline
 def hanoi(n, start, end, other):
     global cnt
     if n == 1:
-        cnt += 1
-        res.append([start, end])
-    else:
-        hanoi(n-1, start, other, end)
-        cnt += 1
-        res.append([start, end])
-        hanoi(n-1, other, end, start)
+        print(start, end)
+        return
+    hanoi(n-1, start, other, end)
+    print(start, end)
+    hanoi(n-1, other, end, start)
 
 
 N = int(input())
-cnt = 0
-res = []
-hanoi(N, 1, 3, 2)
-print(cnt)
-for s, e in res:
-    print(s, e)
+print(2**N - 1)
+if N <= 20:
+    hanoi(N, 1, 3, 2)
