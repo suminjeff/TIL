52d3a7236d71537d73b20acd38067ebcf43b2d73
suminjeff <suminjeff@gmail.com>
2023-11-03 00:04:48 +0900

    231103

--- 00_Ad/practice/색칠 1.py
+++ 00_Ad/practice/색칠 1.py
@@ -1,26 +1,40 @@
 import sys
 sys.stdin = open('색칠 1.txt', 'r')
 input = sys.stdin.readline
-
 W, H, f, c, x1, y1, x2, y2 = map(int, input().split())
 
-arr = [[1]*W + [0]*W for _ in range(H)]
-for i in range(H):
-    for j in range(f, f+f):
-        arr[i][j] += 1
-
-if c > 0:
-    for i in range(c, H, c):
-        for j in range(i, i+c):
-            for k in range(f, W):
-                arr[j][k] += arr[j-c][k]
-
-print(*arr, sep='\n')
-
-for i in range(y1, y2):
-    for j in range(f+x1, f+x2):
-        print(i, j)
+area = (c+1)*(x2-x1)*(y2-y1)
+if f <= W//2:
+    if f <= x1:
+        print(W*H - area)
+    else:
+        print(W*H - (area+(min(f, x2)-x1) * (y2-y1) * (c+1)))
+else:
+    if W <= x1+f:
+        print(W*H - area)
+    else:
+        print(W*H - (area + (min(W, f+x2) - (f+x1)) * (y2-y1)*(c+1)))
 
 
-# [6, 6, 3]
-# [6, 6, 3]
+
+
+# arr = [[1]*W + [0]*W for _ in range(H)]
+# for i in range(H):
+#     for j in range(2*W):
+#         if f <= j < f+f:
+#             arr[i][j] += 1
+#         elif j < f:
+#             arr[i][j] = 0
+# if c > 0:
+#     for i in range(H//(c+1), H, H//(c+1)):
+#         for j in range(i, i+H//(c+1)):
+#             for k in range(f, 2*W):
+#                 arr[j][k] += arr[j-H//(c+1)][k]
+# ans = W * H
+# for i in range(H-y2, H-y1):
+#     for j in range(f+x1, f+x2):
+#         ans -= arr[i][j]
+# print(ans)
+
+
+
