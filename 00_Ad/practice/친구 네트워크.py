08496fe7943930159ff10af0bb0c22451ca659ed
suminjeff <suminjeff@gmail.com>
2023-11-06 00:34:34 +0900

    231106

--- 00_Ad/practice/친구 네트워크.py
+++ 00_Ad/practice/친구 네트워크.py
@@ -12,30 +12,23 @@ def find(x):
 
 def union(x, y):
     x, y = find(x), find(y)
-    parents[max(x, y)] = min(x, y)
-    children[min(x, y)] += 1
-    children[min(x, y)] += children[max(x, y)]
-    children[max(x, y)] = 0
+    if x != y:
+        parents[y] = x
+        children[x] += children[y]
 
 
 T = int(input())
 for tc in range(1, T+1):
     auto_increment = 0
     F = int(input())
-    adj_l = [[] for _ in range(100002)]
-    parents = [i for i in range(100002)]
-    children = [0]*100002
-    pk = {}
+    parents = {}
+    children = {}
     for _ in range(F):
         p1, p2 = input().split()
-        if p1 not in pk.keys():
-            pk.setdefault(p1, auto_increment)
-            auto_increment += 1
-        if p2 not in pk.keys():
-            pk.setdefault(p2, auto_increment)
-            auto_increment += 1
-        adj_l[pk[p1]].append(pk[p2])
-        adj_l[pk[p2]].append(pk[p1])
-        union(pk[p1], pk[p2])
-        print(children[parents[pk[p1]]]+1)
+        if p1 not in parents.keys():
+            parents[p1], children[p1] = p1, 1
+        if p2 not in parents.keys():
+            parents[p2], children[p2] = p2, 1
+        union(p1, p2)
+        print(children[parents[p1]])
 
